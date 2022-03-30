053e9baf0c873386ac04332eefd71dc987316afd
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2022-03-29 22:08:00 -0500

    fixing merge conflicts

--- client/navigators/RootStack.js
+++ client/navigators/RootStack.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Colors } from './../components/styles';
+import { Colors } from '../components/styles';
 const {primary, darkBrick, brick, lightBrick, secondary, tetriary} = Colors;
 
 
-import Login from './../screens/login';
-import Signup from './../screens/signup';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
 import DrawerNavigator from './DrawerNavigator.js';
-import LocationScreen from './../screens/LocationScreen';
+import LocationScreen from '../screens/LocationScreen';
 
 const Stack = createNativeStackNavigator();
 
