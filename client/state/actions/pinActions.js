efba377757a96a9cf984e7d896412483f876d1c2
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2022-04-07 14:25:32 -0500

    started working on pin actions for redux

---
+++ client/state/actions/pinActions.js
@@ -0,0 +1,22 @@
+import pinTypeActions from "./types/pinTypeActions";
+
+export const setSelectedPIn = (pin) => {
+    return {
+        type: pinTypeActions.SET_SELECTED_PIN,
+        payload: pin
+    };
+}
+
+export const setUserSpecificPins = (pins) => {
+    return {
+        type: pinTypeActions.SET_USER_SPECIFIC_PINS,
+        payload: pins
+    };
+}
+
+export const setAllPins = (pins) => {
+    return {
+        type: pinTypeActions.SET_ALL_PINS,
+        payload: pins
+    };
+}
---
+++ client/state/actions/types/pinTypeActions.js
@@ -0,0 +1,7 @@
+const pinTypeActions = {
+    SET_SELECTED_PIN: 'setSelectedPIn',
+    SET_USER_SPECIFIC_PINS: 'setUserSpecificPins',
+    SET_ALL_PINS: 'setAllPins'
+};
+
+export default pinTypeActions;
